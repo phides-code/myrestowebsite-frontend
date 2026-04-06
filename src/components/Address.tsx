9753f4b225e6d77428de2570e715919764559557
Philippe DeSousa <phil.desousa@gmail.com>
2026-04-06 16:39:03 -0400

    show map

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -3,11 +3,24 @@ interface AddressProps {
 }
 
 const Address = ({ address }: AddressProps) => {
+    const addressFormatted = address.replace(' ', '+');
+
     return (
         <div className='page-shell'>
             <h1 className='page-shell__title'>Find Us</h1>
             <div className='address-block'>
                 <p>{address}</p>
+                <iframe
+                    width='100%'
+                    height='300'
+                    loading='lazy'
+                    allowFullScreen
+                    style={{
+                        border: 0,
+                    }}
+                    referrerPolicy='no-referrer-when-downgrade'
+                    src={`https://www.google.com/maps?q=${addressFormatted}&output=embed`}
+                />
             </div>
         </div>
     );
